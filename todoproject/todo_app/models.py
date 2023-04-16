305f631f2414be4195417d762f228af69eda982a
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-04-16 18:34:35 +0300

    Create todo model

--- todoproject/todo_app/models.py
+++ todoproject/todo_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class DateTimeMixin:
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+
+class ToDo(models.Model, DateTimeMixin):
+    title = models.CharField('Задание', max_length=150)
+    description = models.TextField('Описание')
+    deadline = models.DateField('Дата завершения')
+    is_complete = models.BooleanField('Выполнено', default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
